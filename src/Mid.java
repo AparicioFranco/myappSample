38922210c7d12d6be272d353da1324ba89e7861d
Franco Aparicio <franco.aparicio@ing.austral.edu.ar>
2018-08-22 21:38:53 -0300

    Mid class

---
+++ src/Mid.java
@@ -0,0 +1,18 @@
+public class Mid {
+
+
+    public static void showThis(Object object){
+        System.out.println(object);
+    }
+
+
+    public static void main(String[] args) {
+
+
+        Integer inta = 50;
+        
+        showThis(inta);
+
+    }
+
+}
