f60414a68af3294be99726dbb32dc9f3e4f35a37
Franco Aparicio <franco.aparicio@ing.austral.edu.ar>
2018-08-22 21:35:04 -0300

    This

---
+++ src/This.java
@@ -0,0 +1,15 @@
+public class This {
+
+    public static void thisRate(){
+        System.out.println("Im Fekar");
+    }
+
+
+    public static void main(String[] args){
+
+
+        thisRate();
+
+    }
+
+}
