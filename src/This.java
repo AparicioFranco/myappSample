afc121f8d54139f6df4136b69d10ec0fdcea421e
Franco Aparicio <franco.aparicio@ing.austral.edu.ar>
2018-08-22 21:37:14 -0300

    shower class

--- src/This.java
+++ src/This.java
@@ -4,11 +4,24 @@ public class This {
         System.out.println("Im Fekar");
     }
 
+    public static void shower(int[] array){
+        for(int x: array){
+            System.out.println(x);
+        }
+
+
+    }
+
 
     public static void main(String[] args){
 
 
         thisRate();
+
+        int[] array = {3,2,1,6,8};
+        shower(array);
+
+
 
     }
 
